71afb541a49a3b0d028c6af291c485d09180d60e
Prince Eklu <ameto888@protonmail.com>
2024-10-14 03:21:16 -0400

    Add axios connections to Django.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import axios from 'axios'
+
+
 </script>
 
 <template>
